2a4fd322ea6c4277746bec969101c96d53eb5095
Rafael Sikora <rafaelsikora0503@gmail.com>
2023-04-09 21:33:32 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+  //Declaração de variáveis
+int numero1;
+int numero2;
+int soma;
+  //Fazendo a entrada de dados
+Scanner teclado = new Scanner(System.in);
+    System.out.println("Insira o primeiro numero: ");
+    numero1 = teclado.nextInt();
+    System.out.println("Insira o segundo numero: ");
+    numero2 = teclado.nextInt();
+  //Fazendo o calculo
+    soma = numero1 + numero2;
+  //Exibir a resposta ao usuário
+    System.out.println(numero1 + " + " + numero2 + " = " + soma);
+  }
+}
